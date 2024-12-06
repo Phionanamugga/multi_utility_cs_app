f6ba62b850861ce4779eaefef9f5691ca49590c4
Phionanamugga <phionanamugga15@gmail.com>
2024-12-06 23:27:50 +0100

    creates route that enables user edit profile.Delivers#188658815

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 from app.routes.auth import auth_bp
+from app.routes.profile import profile_bp
+
 
 def register_routes(app):
     app.register_blueprint(auth_bp, url_prefix='/auth')
+
+def register_routes(app):
+    app.register_blueprint(profile_bp, url_prefix='/profile')
+
