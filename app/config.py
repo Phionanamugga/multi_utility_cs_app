ccd4a7dbb6ef5245c1bbc311349507ec16a347d9
Phionanamugga <phionanamugga15@gmail.com>
2024-12-08 21:06:40 +0100

    creates customer complaints resolution platform.Delivers#188658826

---
+++ app/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587
+    SMTP_USERNAME = 'your_email@gmail.com'
+    SMTP_PASSWORD = 'your_email_password'
+    SMTP_FROM_EMAIL = 'no-reply@multiutility.com'
