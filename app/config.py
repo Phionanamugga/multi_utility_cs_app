cfbc189d8c9c01030b47f5982a86b315dd8cad6c
Phionanamugga <phionanamugga15@gmail.com>
2024-12-08 21:11:06 +0100

    enables resolving customer complaints via phone.Delivers#188658946

--- app/config.py
+++ app/config.py
@@ -6,3 +6,11 @@ class Config:
     SMTP_USERNAME = 'your_email@gmail.com'
     SMTP_PASSWORD = 'your_email_password'
     SMTP_FROM_EMAIL = 'no-reply@multiutility.com'
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SMS_API_URL = 'https://api.smsprovider.com/send'
+    SMS_API_KEY = 'your_sms_api_key'
+    SMS_SENDER_ID = 'MULTIUTIL'
+
