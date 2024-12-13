8cbe993311ace98964770fdb8929b3176deb425e
Phionanamugga <phionanamugga15@gmail.com>
2024-12-13 03:45:22 +0100

    creates routes for the app

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import Blueprint
 from app.routes.auth import auth_bp
 from app.routes.profile import profile_bp
-from app.routes.user import user_bp
+
 
 def create_app():
     app = Flask(__name__)
@@ -11,7 +11,6 @@ def create_app():
     # Register blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(profile_bp, url_prefix='/profile')
-    app.register_blueprint(user_bp, url_prefix='/user')
-
+    
     return app
 
